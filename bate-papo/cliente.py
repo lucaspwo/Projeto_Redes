11730c08d75c8568c669df26e5795a3d6e491336
Lucas Oliveira <avalon91@users.noreply.github.com>
2017-10-26 12:05:26 -0300

    Setando variável global; substituindo tabs por espaços

--- bate-papo/cliente.py
+++ bate-papo/cliente.py
@@ -1,29 +1,31 @@
 import threading
 from socket import *
 
+global conectado
 
 class recebeMsg (threading.Thread):
-	# redefine a funcao __init__ para aceitar a passagem parametros de entrada
-	def __init__(self,clientSocket):
-		threading.Thread.__init__(self)
-		self.client_Socket = clientSocket
-	# a funcao run() e executada por padrao por cada thread
-	def run(self):
-		#ouvir o que o servidor vai mandar e imprimir em tela
-		while conectado:
-			conectado = True
-			msg = self.client_Socket.recv(2048)
-			print msg
-			if msg == 'sair()':
-				clientSocket.close()
-				conectado = False
+    # redefine a funcao __init__ para aceitar a passagem parametros de entrada
+    def __init__(self,clientSocket):
+        threading.Thread.__init__(self)
+        self.client_Socket = clientSocket
+    # a funcao run() e executada por padrao por cada thread
+    def run(self):
+        global conectado
+        #ouvir o que o servidor vai mandar e imprimir em tela
+        conectado = True
+        while conectado:
+            msg = self.client_Socket.recv(2048)
+            print msg
+            if msg == 'sair()':
+                clientSocket.close()
+                conectado = False
 
 
 
 
 
 #Parte principal
-serverName = '192.168.0.6' #inserir o ip do servidor
+serverName = '127.0.0.1' #inserir o ip do servidor
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -35,12 +37,12 @@ clientSocket.send('nome('+sentence+')')
 thread = recebeMsg (clientSocket)
 thread.start()
 
-#while conectado:
-while True:
-	#Parte para enviar as mensagens
-	sentence = raw_input()
-	clientSocket.send(sentence)
-	if sentence == 'sair()':
-		clientSocket.close()
-		break
+conectado = True
+while conectado == True:
+    #Parte para enviar as mensagens
+    sentence = raw_input()
+    clientSocket.send(sentence)
+    if sentence == 'sair()':
+        clientSocket.close()
+        break
 clientSocket.close()
